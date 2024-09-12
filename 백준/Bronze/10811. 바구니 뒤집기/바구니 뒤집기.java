843513d88ae0e4808ea457cbb7e0b18b94c86a21
Siu Hwang <siuh0403@gmail.com>
2024-09-12 17:38:28 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 100 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,32 +1,24 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int n,m;
     public static ArrayList<Integer> bucket = new ArrayList<>();
     
     public static void reverseBucket(int x, int y) {
-        
-        x = x -1;
-        
-        ArrayList<Integer> subList = new ArrayList<>(bucket.subList(x, y));
-        Collections.reverse(subList);
-        
-        for(int i = 0; i < subList.size(); i++) {
-            bucket.set(x+i, subList.get(i));
+        x = x - 1;
+        ArrayList<Integer> subBucket = new ArrayList<>(bucket.subList(x, y));
+        Collections.reverse(subBucket);
+        for(int i = 0; i < subBucket.size(); i++) {
+            bucket.set(i + x, subBucket.get(i));
         }
     }
     
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
-       
-        n = Integer.parseInt(stk.nextToken());
-        m = Integer.parseInt(stk.nextToken());
+        
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
         
         for(int i = 1; i <= n; i++) {
             bucket.add(i);
@@ -36,7 +28,6 @@ public class Main {
             stk = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(stk.nextToken());
             int y = Integer.parseInt(stk.nextToken());
-            
             reverseBucket(x, y);
         }
         
