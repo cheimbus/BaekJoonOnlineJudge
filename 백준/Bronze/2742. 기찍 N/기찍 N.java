419dcb957070db5cd1bca4dcc15270ca12583558
Siu Hwang <siuh0403@gmail.com>
2024-09-13 09:27:58 +0900

    [Bronze IV] Title: 기찍 N, Time: 184 ms, Memory: 18168 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = n; i >= 1; i--) {
+            sb.append(i).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
