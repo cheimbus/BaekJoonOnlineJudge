62bac409a8c741bdcd80efc0b4c7ee2e9fcdc0ad
Siu Hwang <siuh0403@gmail.com>
2024-09-16 14:10:01 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 96 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -2,50 +2,43 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-
 public class Main {
-    public static String[] commendArr;
-    public static ArrayList<Character> charLi = new ArrayList<>();
+    public static String[] commendLi;
+    public static ArrayList<Character> lastCommend = new ArrayList<>();
     
     public static void findCommend() {
-        int length = commendArr[0].length();  // 첫 번째 문자열의 길이를 기준으로
-        for (int i = 0; i < length; i++) {
-            char currentChar = commendArr[0].charAt(i);  // 첫 번째 문자열의 i번째 문자
+        for(int i = 0 ; i < commendLi[0].length(); i++) {
+            char curCommend = commendLi[0].charAt(i);
             boolean isSame = true;
             
-            // 다른 문자열들과 비교
-            for (int j = 1; j < commendArr.length; j++) {
-                if (commendArr[j].charAt(i) != currentChar) {
+            for(int j = 1; j < commendLi.length; j++) {
+                if(curCommend != commendLi[j].charAt(i)) {
                     isSame = false;
+                    lastCommend.add('?');
                     break;
                 }
             }
-            
-            if (isSame) {
-                charLi.add(currentChar);  // 모두 같으면 해당 문자를 추가
-            } else {
-                charLi.add('?');  // 다르면 '?' 추가
+            if(isSame) {
+                lastCommend.add(curCommend);
             }
         }
     }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());  // 문자열의 개수
         
-        commendArr = new String[n];  // n개의 문자열을 담을 배열 생성
-        
-        for (int i = 0; i < n; i++) {
-            commendArr[i] = br.readLine();  // 각 문자열 입력받기
+        int n = Integer.parseInt(br.readLine());
+        commendLi = new String[n];
+        for(int i = 0; i < n; i++) {
+            commendLi[i] = br.readLine();
         }
         
-        findCommend();  // 공통 패턴 찾기
+        findCommend();
         
         StringBuilder sb = new StringBuilder();
-        
-        for (Character ch : charLi) {
-            sb.append(ch);  // 결과 문자열 생성
+        for(char c : lastCommend) {
+            sb.append(c);
         }
-        System.out.print(sb);  // 출력
+        System.out.print(sb);
     }
 }
