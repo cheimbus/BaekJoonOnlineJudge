007b32f23961a528e04bb8b7247da6c04c505440
Siu Hwang <siuh0403@gmail.com>
2024-10-23 22:59:18 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 120 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        int ans = n * m - 1;
+
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
