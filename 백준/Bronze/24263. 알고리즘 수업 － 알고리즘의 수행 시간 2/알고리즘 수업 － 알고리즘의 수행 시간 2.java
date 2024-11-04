d5236bf0d44ebf2ad3a5be0d77199ffe67647a42
Siu Hwang <siuh0403@gmail.com>
2024-11-04 20:32:05 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        sb.append(n).append("\n").append(1);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
