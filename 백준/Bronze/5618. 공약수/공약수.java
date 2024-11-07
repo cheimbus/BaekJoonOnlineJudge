c37f47ce508ec83601c5a7aa8dfaedbf452c36c3
Siu Hwang <siuh0403@gmail.com>
2024-11-07 18:41:41 +0900

    [Bronze II] Title: 공약수, Time: 452 ms, Memory: 17900 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.java
+++ 백준/Bronze/5618. 공약수/공약수.java
@@ -11,28 +11,24 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[3];
 
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i ++) {
-            int num = Integer.parseInt(stk.nextToken());
-            arr[i] = num;
-        }
-
-        int maxVal = -1;
-        for(int i : arr) {
-            if(maxVal < i) maxVal = i;
-        }
-
         StringBuilder sb = new StringBuilder();
         if(n == 2) {
-            for(int i = 1; i <= maxVal; i ++) {
-                if(arr[0] % i == 0 && arr[1] % i == 0) sb.append(i).append("\n");
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+
+            for(int i = 1; i <= Math.min(a, b); i ++) {
+                if(a % i == 0 && b % i == 0) sb.append(i).append("\n");
             }
         }
         else if(n == 3) {
-            for(int i = 1; i <= maxVal; i ++) {
-                if(arr[0] % i == 0 && arr[1] % i == 0 && arr[2] % i == 0) sb.append(i).append("\n");
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+            int c = Integer.parseInt(stk.nextToken());
+
+            for(int i = 1; i <= Math.min(Math.min(a, b), c); i ++) {
+                if(a % i == 0 && b % i == 0 && c % i == 0) sb.append(i).append("\n");
             }
         }
 
