822cbee6ea30cecf21624f301776b99f840bd56a
Siu Hwang <siuh0403@gmail.com>
2024-11-27 10:51:51 +0900

    [Bronze V] Title: N 찍기, Time: 184 ms, Memory: 18176 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= n; i ++) {
+            sb.append(i).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+    }
+}
