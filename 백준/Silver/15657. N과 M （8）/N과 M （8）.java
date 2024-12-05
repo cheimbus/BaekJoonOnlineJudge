2cdf2d36ceff146464c70708c89e6c9f6fd7ed18
Siu Hwang <siuh0403@gmail.com>
2024-12-05 16:21:39 +0900

    [Silver III] Title: N과 M (8), Time: 144 ms, Memory: 17948 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static int n, m;
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] arr;
+    public static int[] tmp;
+
+    public static void dfs(int depth, int at) {
+        if(depth == m) {
+            for(int i = 0; i < tmp.length; i ++) {
+                sb.append(tmp[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = at; i < n; i ++) {
+            tmp[depth] = arr[i];
+            dfs(depth + 1, i);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+
+        arr = new int[n];
+        tmp = new int[m];
+
+        stk = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i ++) {
+            arr[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
