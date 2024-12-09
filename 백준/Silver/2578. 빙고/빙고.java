aa58a90b1cb411bf5ffdb4da677432265a998c2a
Siu Hwang <siuh0403@gmail.com>
2024-12-09 14:55:24 +0900

    [Silver IV] Title: 빙고, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -6,73 +6,66 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[][] grid = new int[5][5];
-    public static int[][] tmpGrid = new int[5][5];
-    public static boolean[][] visited = new boolean[5][5];
-    public static int cnt;
+    public static int[][] grid;
+    public static int[][] tmp;
+    public static boolean[][] visited;
+    public static int ans;
 
     public static void check(int val) {
         for(int i = 0; i < 5; i ++) {
             for(int j = 0; j < 5; j ++) {
                 if(val == grid[i][j]) {
                     visited[i][j] = true;
-                    return;
                 }
             }
         }
     }
 
-    public static boolean bingo() {
-        int bingoCnt = 0;
+    public static boolean find() {
+        int res = 0;
 
         for(int i = 0; i < 5; i ++) {
-            int cnt1 = 0;
+            int cnt = 0;
             for(int j = 0; j < 5; j ++) {
-                if(visited[i][j]) {
-                    cnt1 ++;
-                    if(cnt1 == 5) bingoCnt ++;
-                }
+                if(visited[i][j]) cnt ++;
+            }
+            if(cnt == 5) {
+                res ++;
             }
         }
 
         for(int i = 0; i < 5; i ++) {
-            int cnt1 = 0;
+            int cnt = 0;
             for(int j = 0; j < 5; j ++) {
-                if(visited[j][i]) {
-                    cnt1 ++;
-                    if(cnt1 == 5) bingoCnt ++;
-                }
+                if(visited[j][i]) cnt ++;
             }
+            if(cnt == 5) res ++;
         }
+
+        int cnt1 = 0;
+        for(int i = 0; i < 5; i ++) {
+            if(visited[i][i]) cnt1 ++;
+        }
+        if(cnt1 == 5) res ++;
 
         int cnt2 = 0;
-        for(int i = 0; i < 5; i ++) {
-            if(visited[i][i]) {
-                cnt2 ++;
-                if(cnt2 == 5) bingoCnt ++;
-            }
+        for(int i = 4; i >= 0; i --) {
+            if(visited[i][4 - i]) cnt2 ++;
         }
+        if(cnt2 == 5) res ++;
 
-        int cnt3 = 0;
-        for(int i = 0; i < 5; i ++) {
-            if(visited[i][4 - i]) {
-                cnt3 ++;
-                if(cnt3 == 5) bingoCnt ++;
-            }
-        }
-
-        if(bingoCnt >= 3) return true;
+        if(res >= 3) return true;
         else return false;
     }
 
     public static void impl() {
         for(int i = 0; i < 5; i ++) {
             for(int j = 0; j < 5; j ++) {
-                int val = tmpGrid[i][j];
-                cnt ++;
+                int val = tmp[i][j];
                 check(val);
+                ans ++;
 
-                if(bingo()) return;
+                if(find()) return;
             }
         }
     }
@@ -80,6 +73,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        grid = new int[5][5];
+        tmp = new int[5][5];
+        visited = new boolean[5][5];
 
         for(int i = 0; i < 5; i ++) {
             StringTokenizer stk = new StringTokenizer(br.readLine());
@@ -91,13 +88,14 @@ public class Main {
         for(int i = 0; i < 5; i ++) {
             StringTokenizer stk = new StringTokenizer(br.readLine());
             for(int j = 0; j < 5; j ++) {
-                tmpGrid[i][j] = Integer.parseInt(stk.nextToken());
+                tmp[i][j] = Integer.parseInt(stk.nextToken());
             }
         }
 
         impl();
 
-        bw.write(cnt + "");
+        bw.write(ans + "");
         bw.flush();
+        bw.close();
     }
 }
