6cb447948bb33c02520b1126568d10e46467c094
Siu Hwang <siuh0403@gmail.com>
2024-12-10 15:13:11 +0900

    [Silver IV] Title: 지뢰 찾기, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.java
+++ 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.java
@@ -18,49 +18,41 @@ public class Main {
         return 0 <= x && 0 <= y && x < n && y < n;
     }
 
-    public static boolean isBomb(int x, int y) {
+    public static boolean bomb(int x, int y) {
         return grid[x][y] == '*';
     }
 
-    public static void find(int x, int y, char val) {
+    public static void check(int x, int y, char c) {
         int cnt = 0;
-        if(val == 'x') {
+        if(c == 'x') {
+            if(grid[x][y] == '*') flag = true;
+
             for(int i = 0; i < 8; i ++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if(inRange(nx, ny) && isBomb(nx, ny)) {
+                if(inRange(nx, ny) && bomb(nx, ny)) {
                     cnt ++;
                 }
-                if(grid[x][y] == '*') flag = true;
             }
+            ans[x][y] = (char) (cnt + '0');
         }
-        else cnt = -1;
-
-        if(cnt >= 0) ans[x][y] = (char) (cnt + '0');
-        else ans[x][y] = '.';
-    }
-
-    public static void check(int x, int y) {
-        ans[x][y] = '*';
+        else {
+            ans[x][y] = c;
+        }
     }
 
     public static void impl() {
         for(int i = 0; i < n; i ++) {
             for(int j = 0; j < n; j ++) {
-                char val = tmp[i][j];
-                find(i, j, val);
+                char c = tmp[i][j];
+                check(i, j, c);
             }
         }
 
-        if(flag) {
-            for(int i = 0; i < n; i ++) {
-                for(int j = 0; j < n; j ++) {
-                    if(grid[i][j] == '*') {
-                        check(i, j);
-                    }
-                }
+        for(int i = 0; i < n; i ++)
+            for(int j = 0; j < n; j ++) {
+                if(grid[i][j] == '*' && flag) ans[i][j] = '*';
             }
-        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -68,29 +60,30 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = Integer.parseInt(br.readLine());
+
         grid = new char[n][n];
         tmp = new char[n][n];
         ans = new char[n][n];
 
         for(int i = 0; i < n; i ++) {
-            char[] s = br.readLine().toCharArray();
+            char[] cArr = br.readLine().toCharArray();
             for(int j = 0; j < n; j ++) {
-                grid[i][j] = s[j];
+                grid[i][j] = cArr[j];
             }
         }
 
         for(int i = 0; i < n; i ++) {
-            char[] s = br.readLine().toCharArray();
+            char[] cArr = br.readLine().toCharArray();
             for(int j = 0; j < n; j ++) {
-                tmp[i][j] = s[j];
+                tmp[i][j] = cArr[j];
             }
         }
 
         impl();
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < n; i ++) {
-            for(int j = 0; j < n; j ++) {
+        for(int i = 0; i < ans.length; i ++) {
+            for(int j = 0; j < ans.length; j ++) {
                 sb.append(ans[i][j]);
             }
             sb.append("\n");
