4e4dfaf89037b6b46888c25009485e24e175109c
Siu Hwang <siuh0403@gmail.com>
2024-12-16 23:07:22 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 100 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,9 +1,5 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static int n, m;
@@ -13,33 +9,31 @@ public class Main {
         return 0 <= x && 0 <= y && x < n && y < n;
     }
 
-    public static void impl(int a, int b) {
-        if(a == 1) {
-            for(int i = b; i < n; i += b + 1) {
+    public static void impl(int x, int y) {
+        if(x == 1) {
+            for(int i = y; i < n; i += y + 1) {
                 int val = arr[i];
-                if(val == 1) arr[i] = 0;
+                if(val == 1) {
+                    arr[i] = 0;
+                }
                 else arr[i] = 1;
             }
         }
         else {
-            int nb1 = b;
-            int nb2 = b;
-            while(inRange(nb1, nb2)) {
-                int nx = arr[nb1];
-                int ny = arr[nb2];
-                if(nx != ny) break;
-                else {
-                    if(nx == 1 && ny == 1) {
-                        arr[nb1] = 0;
-                        arr[nb2] = 0;
-                    }
-                    else {
-                        arr[nb1] = 1;
-                        arr[nb2] = 1;
-                    }
+            int nx = y;
+            int ny = y;
+
+            while(inRange(nx, ny) && arr[nx] == arr[ny]) {
+                if(arr[nx] == 1 && arr[ny] == 1) {
+                    arr[nx] = 0;
+                    arr[ny] = 0;
                 }
-                nb1 -= 1;
-                nb2 += 1;
+                else {
+                    arr[nx] = 1;
+                    arr[ny] = 1;
+                }
+                nx -= 1;
+                ny += 1;
             }
         }
     }
@@ -50,32 +44,34 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
-
         StringTokenizer stk = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i ++) {
             arr[i] = Integer.parseInt(stk.nextToken());
         }
 
         m = Integer.parseInt(br.readLine());
+
         while(m -- > 0) {
             stk = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(stk.nextToken());
-            int b = Integer.parseInt(stk.nextToken());
-            impl(a, b - 1);
+            int x = Integer.parseInt(stk.nextToken());
+            int y = Integer.parseInt(stk.nextToken());
+
+            impl(x, y - 1);
         }
 
         StringBuilder sb = new StringBuilder();
-
         int cnt = 0;
-        for(int i = 0; i < arr.length; i ++) {
-            if(cnt == 20) {
-                sb.append("\n");
-                cnt = 0;
-            }
-            cnt ++;
+        for(int i = 0; i < n; i ++) {
             sb.append(arr[i]).append(" ");
+            cnt ++;
+            if(cnt == 20) {
+                cnt = 0;
+                sb.append("\n");
+            }
         }
+
         bw.write(sb.toString());
         bw.flush();
+
     }
 }
