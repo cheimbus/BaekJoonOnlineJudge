b6284a8da9f461c30b24908102d86ca02f994f94
Siu Hwang <siuh0403@gmail.com>
2024-12-17 00:39:44 +0900

    [Silver III] Title: 달팽이, Time: 476 ms, Memory: 51460 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -1,9 +1,5 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static int n, m;
@@ -15,6 +11,30 @@ public class Main {
         return 0 <= x && 0 <= y && x < n && y < n;
     }
 
+    public static void impl() {
+        int x = n / 2;
+        int y = n / 2;
+        int idx = 0;
+        int num = 1;
+
+        grid[x][y] = num;
+
+        while(true) {
+            int nx = x + dx[idx];
+            int ny = y + dy[idx];
+
+            if(!inRange(nx, ny)) break;
+
+            if(idx != 3) {
+                if(grid[nx + dx[idx + 1]][ny + dy[idx + 1]] == 0) idx ++;
+            }
+            else if(grid[nx + dx[0]][ny + dy[0]] == 0) idx = 0;
+            x = nx;
+            y = ny;
+            grid[nx][ny] = ++ num;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -24,41 +44,24 @@ public class Main {
 
         grid = new int[n][n];
 
-        int nx = n / 2, ny = n / 2;
-        int num = 1;
-        int idx = 0;
-        grid[nx][ny] = num;
-
-        while(true) {
-            nx += dx[idx];
-            ny += dy[idx];
-
-            if(!inRange(nx, ny)) break;
-
-            if(idx != 3) {
-                if(grid[nx + dx[idx + 1]][ny + dy[idx + 1]] == 0) idx ++;
-            }
-            else {
-                if(grid[nx + dx[0]][ny + dy[0]] == 0) idx = 0;
-            }
-
-            grid[nx][ny] = ++num;
-        }
+        impl();
 
         StringBuilder sb = new StringBuilder();
-        int x = 0, y = 0;
+        int x = 0;
+        int y = 0;
         for(int i = 0; i < n; i ++) {
             for(int j = 0; j < n; j ++) {
-                if(grid[i][j] == m) {
-                    x = i + 1; y = j + 1;
-                }
                 sb.append(grid[i][j]).append(" ");
+                if(m == grid[i][j]) {
+                    x = i + 1;
+                    y = j + 1;
+                }
             }
             sb.append("\n");
         }
-        sb.append(x + " " + y);
-
+        sb.append(x + " ").append(y);
         bw.write(sb.toString());
         bw.flush();
+
     }
 }
