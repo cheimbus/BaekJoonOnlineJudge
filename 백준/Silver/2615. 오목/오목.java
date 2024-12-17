3f4192418c37fd44acbbc1ccdaeab12f636f701c
Siu Hwang <siuh0403@gmail.com>
2024-12-17 16:38:21 +0900

    [Silver I] Title: 오목, Time: 116 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -2,34 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] map = new int[21][21];
-    static int[][][] memo = new int[21][21][4];
+    public static StringBuilder sb = new StringBuilder();
+    public static int[][] grid = new int[19][19];
+    public static int[][][] visited = new int[19][19][4];
+    public static int[] dx = new int[]{1, 1, 0, -1};
+    public static int[] dy = new int[]{0, 1, 1, 1};
 
-    static int[] dx = { 1, 1, 0, -1 };
-    static int[] dy = { 0, 1, 1, 1 };
-
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        for(int i=1; i<=19; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=1; j<=19; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        System.out.println(findFive());
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && 0 <= y && x < 19 && y < 19;
     }
 
-    private static String findFive() {
-        for (int j = 1; j <= 19; j++) {
-            for (int i = 1; i <= 19; i++) {
-                if (map[i][j] != 0) {
-                    for (int d = 0; d < 4; d++) {
-                        if (memo[i][j][d] == 0 && calc(i, j, d, map[i][j]) == 5) {
-                            return map[i][j] + "\n" + i + " " + j + "\n";
+    public static int calc(int x, int y, int dir, int val) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if(inRange(nx, ny) && grid[nx][ny] == val) {
+            return visited[nx][ny][dir] = calc(nx, ny, dir, val) + 1;
+        }
+
+        return 1;
+    }
+
+    public static String impl() {
+        for(int j = 0; j < 19; j ++) {
+            for(int i = 0; i < 19; i ++) {
+                if(grid[i][j] != 0) {
+                    for(int d = 0; d < 4; d ++) {
+                        if(visited[i][j][d] == 0 && calc(i, j, d, grid[i][j]) == 5) {
+                            sb.append(grid[i][j] + "\n").append(i + 1 +" ").append(j + 1);
+                            return sb.toString();
                         }
                     }
                 }
@@ -38,13 +39,18 @@ public class Main {
         return "0";
     }
 
-    private static int calc(int x, int y, int dir, int color) {
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        if (map[nx][ny] == color) {
-            return memo[nx][ny][dir] = calc(nx, ny, dir, color) + 1;
+        for(int i = 0; i < 19; i ++) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            for(int j = 0; j < 19; j ++) {
+                grid[i][j] = Integer.parseInt(stk.nextToken());
+            }
         }
-        return 1;
+
+        bw.write(impl());
+        bw.flush();
     }
 }
