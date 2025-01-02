82be7162fe94456aa161bb2285f0e213164d74dd
Siu Hwang <siuh0403@gmail.com>
2025-01-02 15:58:54 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int[] arr;
+    public static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        dp = new int[n + 1];
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i ++) {
+            arr[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        dp[0] = 0;
+
+        for(int i = 1; i <= n; i ++) {
+            for(int j = i - 1; j >= 0; j --) {
+                if(arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 1; i <= n; i ++) {
+            ans = Math.max(dp[i], ans);
+        }
+
+        bw.write(ans + "");
+        bw.flush();
+    }
+}
