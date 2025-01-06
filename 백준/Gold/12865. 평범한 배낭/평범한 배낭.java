980b961974948f4be7955b3349272fe5ed965fc7
Siu Hwang <siuh0403@gmail.com>
2025-01-06 14:31:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 152 ms, Memory: 17520 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int k = Integer.parseInt(stk.nextToken());
+
+        int[] arr1 = new int[n + 1];
+        int[] arr2 = new int[n + 1];
+        int[] dp = new int[k + 1];
+
+        for(int i = 1; i <= n; i ++) {
+            stk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+            arr1[i] = a;
+            arr2[i] = b;
+        }
+
+        for(int i = 1; i <= n; i ++) {
+            for(int j = k; j >= 1; j --) {
+                if(j - arr1[i] >= 0) {
+                    dp[j] = Math.max(dp[j], dp[j - arr1[i]] + arr2[i]);
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 1; i <= k; i ++) {
+            ans = Math.max(ans, dp[i]);
+        }
+        bw.write(ans + "");
+        bw.flush();
+    }
+}
