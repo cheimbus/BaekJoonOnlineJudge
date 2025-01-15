1e2f56e8daeef0ca78814bc6e946f6639949ec47
Siu Hwang <siuh0403@gmail.com>
2025-01-15 09:50:09 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 396 ms, Memory: 30624 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
@@ -3,34 +3,30 @@ import java.util.*;
 
 public class Main {
     public static int n, m, cnt;
-    public static List<Integer>[] graph;
+    public static ArrayList<Integer>[] graph;
+    public static ArrayList<Integer> arr = new ArrayList<>();
     public static boolean[] visited;
     public static StringBuilder sb = new StringBuilder();
-    public static List<Integer> tmp = new ArrayList<>();
-
-    public static boolean possible(int node) {
-        for (int t : tmp) {
-            if (graph[t].contains(node)) {
-                return false;
-            }
+    public static boolean possible(int val) {
+        for(int i = 0; i < arr.size(); i ++) {
+            if(graph[val].contains(arr.get(i))) return false;
         }
         return true;
     }
 
     public static void dfs(int depth, int at) {
-        if (depth == 3) {
-            sb.append("\n");
-            cnt++;
+        if(depth == 3) {
+            cnt ++;
             return;
         }
 
-        for (int i = at; i <= n; i++) {
-            if (!visited[i] && possible(i)) {
+        for(int i = at; i <= n; i ++) {
+            if(!visited[i] && possible(i)) {
+                arr.add(i);
                 visited[i] = true;
-                tmp.add(i);
-                dfs(depth + 1, i + 1);
-                tmp.remove(tmp.size() - 1);
+                dfs(depth + 1, i);
                 visited[i] = false;
+                arr.remove(arr.size() - 1);
             }
         }
     }
@@ -38,18 +34,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer stk = new StringTokenizer(br.readLine());
 
+        StringTokenizer stk = new StringTokenizer(br.readLine());
         n = Integer.parseInt(stk.nextToken());
         m = Integer.parseInt(stk.nextToken());
+
         graph = new ArrayList[n + 1];
         visited = new boolean[n + 1];
-
-        for (int i = 1; i <= n; i++) {
+        for(int i = 1; i <= n; i ++) {
             graph[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < m; i++) {
+        for(int i = 0; i < m; i ++) {
             stk = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(stk.nextToken());
             int b = Integer.parseInt(stk.nextToken());
