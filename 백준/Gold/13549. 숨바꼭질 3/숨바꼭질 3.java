cd3b59d98201c1ad01b7e45487b9dbac70b22dce
Siu Hwang <siuh0403@gmail.com>
2025-02-19 14:28:21 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 228 ms, Memory: 87176 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+class Tuple {
+    int x;
+    int y;
+    public Tuple(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    public static int n, m;
+    public static int sec = Integer.MAX_VALUE;
+    public static int max = 100000;
+    public static boolean[] visited;
+    public static Queue<Tuple> q = new LinkedList<>();
+
+    public static void bfs() {
+        while(!q.isEmpty()) {
+            Tuple t = q.poll();
+            int x = t.x;
+            int y = t.y;
+            visited[x] = true;
+
+            if(x == m) sec = Math.min(sec, y);
+            if(x * 2 <= max && !visited[x * 2]) q.add(new Tuple(2 * x, y));
+            if(x + 1 <= max && !visited[x + 1]) q.add(new Tuple(x + 1, y + 1));
+            if(x - 1 >= 0 && !visited[x - 1]) q.add(new Tuple(x - 1, y + 1));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+        visited = new boolean[max + 1];
+
+        q.add(new Tuple(n, 0));
+        bfs();
+
+        bw.write(sec + "");
+        bw.flush();
+        br.close();
+    }
+}
