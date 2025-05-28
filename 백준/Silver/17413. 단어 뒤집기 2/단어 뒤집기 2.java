988b866dbcf9a6026911ab35d2b55bacac71fc4b
Siu Hwang <siuh0403@gmail.com>
2025-05-28 15:13:55 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 200 ms, Memory: 22864 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -2,52 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static char[] arr;
-    public static StringBuilder sb = new StringBuilder();
     public static Stack<Character> st = new Stack<>();
+    public static boolean flag;
+    public static StringBuilder sb = new StringBuilder();
 
-    public static void print() {
+    public static void pop() {
         while(!st.isEmpty()) {
             sb.append(st.pop());
         }
-    }
-
-    public static void impl() {
-        boolean flag = false;
-        for(int i = 0; i < arr.length; i ++) {
-            char val = arr[i];
-            if(val == '<') {
-                print();
-                sb.append('<');
-                flag = true;
-            }
-            else if(val == '>') {
-                sb.append('>');
-                flag = false;
-            }
-            else if(flag) {
-                sb.append(val);
-            }
-            else {
-                if(val == ' ') {
-                    print();
-                    sb.append(' ');
-                }
-                else {
-                    st.push(val);
-                }
-            }
-        }
-        print();
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        arr = br.readLine().toCharArray();
+        char[] cArr = br.readLine().toCharArray();
 
-        impl();
+        for(char c : cArr) {
+            if(c == '<') {
+                pop();
+                flag = true;
+                sb.append(c);
+            }
+            else if(c == '>') {
+                flag = false;
+                sb.append(c);
+            }
+            else if(flag) {
+                sb.append(c);
+            }
+            else {
+                if(c == ' ') {
+                    pop();
+                    sb.append(' ');
+                }
+                else {
+                    st.add(c);
+                }
+            }
+        }
+        pop();
 
         bw.write(sb.toString());
         bw.flush();
