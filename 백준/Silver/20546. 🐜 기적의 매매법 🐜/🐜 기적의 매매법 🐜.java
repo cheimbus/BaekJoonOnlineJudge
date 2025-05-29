cf78ba9eaf65d081b810fe9818e5f45f906945cb
Siu Hwang <siuh0403@gmail.com>
2025-05-29 16:06:37 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
@@ -1,57 +1,53 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
     public static int n;
-    public static int[] arr = new int[14];
+    public static int[] arr;
 
-    public static int find1() {
+    public static int impl1() {
         int val = n;
-        int result = 0;
-        int lastVal = 0;
-        for(int i = 0; i < arr.length; i ++) {
-            if(val / arr[i] > 0) {
-                result += val / arr[i];
-                val = val % arr[i];
+        int re = 0;
+
+        for(int i = 0; i < 14; i ++) {
+            if(val / arr[i] != 0) {
+                re += val / arr[i];
+                val %= arr[i];
             }
-            lastVal = arr[i];
         }
-        return val + result * lastVal;
+        return val + re * arr[13];
     }
 
-    public static int find2() {
+    public static int impl2() {
         int val = n;
-        int remain = 0;
-        int currVal = arr[0];
-        int aCnt = 1;
-        int bCnt = 1;
+        int re = 0;
+        int a = arr[0];
+        int cnt1 = 0;
+        int cnt2 = 0;
 
-        for(int i = 1; i < arr.length; i ++) {
-            if(currVal > arr[i]) {
-                aCnt ++;
-                bCnt = 0;
-                if(aCnt == 3) {
-                    remain += val / arr[i];
+        for(int i = 1; i < 14; i ++) {
+            if(a < arr[i]) {
+                cnt1 ++;
+                cnt2 = 0;
+                if(cnt1 == 3) {
+                    cnt1 = 0;
+                    val += re * arr[i];
+                    re = 0;
+                }
+            }
+            else if(a > arr[i]) {
+                cnt2 ++;
+                cnt1 = 0;
+                if(cnt2 == 3) {
+                    cnt2 = 0;
+                    re += val / arr[i];
                     val %= arr[i];
-                    aCnt = 2;
                 }
             }
-            else if(currVal < arr[i]) {
-                bCnt ++;
-                aCnt = 0;
-                if(bCnt == 3) {
-                    val += remain * arr[i];
-                    remain = 0;
-                    bCnt = 2;
-                }
-            }
-            currVal = arr[i];
+            a = arr[i];
         }
-        return val + remain * currVal;
+        return val + re * a;
     }
 
     public static void main(String[] args) throws IOException {
@@ -59,14 +55,14 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = Integer.parseInt(br.readLine());
-
+        arr = new int[14];
         StringTokenizer stk = new StringTokenizer(br.readLine());
         for(int i = 0; i < 14; i ++) {
             arr[i] = Integer.parseInt(stk.nextToken());
         }
 
-        int a = find1();
-        int b = find2();
+        int a = impl1();
+        int b = impl2();
 
         StringBuilder sb = new StringBuilder();
         if(a > b) sb.append("BNP");
